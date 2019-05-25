2bad1f0ee6e1f221107f3fe5c34391d6dbcfc102
ayesdie <ayushchamoli8@gmail.com>
2019-05-25 16:00:14 +0530

    Initializing the Project

---
+++ include/alt.hpp
@@ -0,0 +1,22 @@
+
+#ifndef ALT_HPP
+#define ALT_HPP
+
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <cctype>
+#include <climits>
+#include <cfloat>
+#include <cstdint>
+#include <stdexcept>
+#include <tuple>
+#include <utility>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include "alt_bits/log.hpp"
+
+#endif
