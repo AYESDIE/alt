39ce5f62d75b2ce9e07721eb6297c626e038a7b9
ayesdie <ayushchamoli8@gmail.com>
2019-05-25 17:10:19 +0530

    hotfix

--- tests/main.cpp
+++ tests/main.cpp
@@ -7,7 +7,7 @@
 
 //#define CATCH_CONFIG_MAIN  // catch.hpp will define main()
 #define CATCH_CONFIG_RUNNER  // we will define main()
-#include "catch.cpp"
+#include "catch.hpp"
 
 int main(int argc, char** argv)
 {
